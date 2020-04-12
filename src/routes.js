3445181a0ad3c79598191e5d5f8f30a904a10e2e
citnes <felipedelta0@gmail.com>
2020-04-12 13:33:16 -0300

    04# Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello, World!' })
-);
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'Luis Felipe',
+    email: 'felipedelta0@gmail.com',
+    password_hash: '123456789',
+  });
+
+  response.json(user);
+});
 
 export default routes;
